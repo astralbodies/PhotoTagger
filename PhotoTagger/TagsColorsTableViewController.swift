80680a25f6ceac62c9e492e08d50bcfc65f52f6b
Aaron Douglas <astralbodies@gmail.com>
2015-11-15 21:27:39 -0600

    Color fixes

--- PhotoTagger/TagsColorsTableViewController.swift
+++ PhotoTagger/TagsColorsTableViewController.swift
@@ -33,8 +33,16 @@ class TagsColorsTableViewController: UITableViewController {
         let cellData = data?[indexPath.row]
         
         if let color = cellData?.color {
+            var red = CGFloat(0.0), green = CGFloat(0.0), blue = CGFloat(0.0), alpha = CGFloat(0.0)
+            color.getRed(&red, green: &green, blue: &blue, alpha: &alpha)
+            let threshold = CGFloat(105)
+            let bgDelta = ((red * 0.299) + (green * 0.587) + (blue * 0.114));
+            
+            let textColor = (255 - bgDelta < threshold) ? UIColor.blackColor() : UIColor.whiteColor();
+            cell.textLabel?.textColor = textColor
             cell.backgroundColor = color
         } else {
+            cell.textLabel?.textColor = UIColor.blackColor()
             cell.backgroundColor = UIColor.whiteColor()
         }
     }
