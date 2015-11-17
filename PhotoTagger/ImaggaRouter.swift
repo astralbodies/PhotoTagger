9fd3d941743ec4c234a76fbc59e5c7713277b726
Aaron Douglas <astralbodies@gmail.com>
2015-11-17 06:54:51 -0600

    Fixed basic auth issue

--- PhotoTagger/ImaggaRouter.swift
+++ PhotoTagger/ImaggaRouter.swift
@@ -49,7 +49,7 @@ public enum ImaggaRouter: URLRequestConvertible {
         let URL = NSURL(string: ImaggaRouter.baseURLPath)!
         let URLRequest = NSMutableURLRequest(URL: URL.URLByAppendingPathComponent(result.path))
         URLRequest.HTTPMethod = result.method.rawValue
-        URLRequest.setValue("Basic \(ImaggaRouter.authenticationToken)", forHTTPHeaderField: "Authorization")
+        URLRequest.setValue(ImaggaRouter.authenticationToken, forHTTPHeaderField: "Authorization")
         URLRequest.timeoutInterval = NSTimeInterval(10 * 1000)
 
         let encoding = Alamofire.ParameterEncoding.URL
